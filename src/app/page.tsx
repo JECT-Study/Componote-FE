dfab0d2be28c10c3c9cc5e32b9a0dd3439d74b14
jiyeeeah <sije3757@snu.ac.kr>
2024-12-14 20:40:53 +0900

    [🔨chore]: 디자인 토큰 확정 파일 파싱 로직 구현
    - test로 구현해둔 로직이 불필요해짐 판단하여 함수 제거
    - 받은 디자인 토큰 파일 중 단 하나만 사용함
    - 기존의 sample 파일 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="bg-light-fill_theme_amber_bolder">Home</div>;
+  return <div className="bg-light-fillNormal">Home</div>;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,34 +27,13 @@ interface DesignTokens {
 }
 
 // TODO: 현재 파일 가져오는 주소 sample파일 주소로 되어있음 => 추후 최종 파일 받으면 경로 수정하기
-const designTokensPath = path.resolve("./src/designToken/", "componoteSampleTokens.json");
+const designTokensPath = path.resolve(
+  "./src/designToken/",
+  "colorTokens(mode).json"
+);
 const rawData = fs.readFileSync(designTokensPath, "utf-8");
 const designTokensString = JSON.parse(rawData);
 const designTokens: DesignTokens = JSON.parse(designTokensString); // 파싱 두번 해야 정상적으로 파싱됨 raw -> string -> json
-
-/**
- * tokens을 가지고 ThemeColors 객체로 변환
- * @param tokens 디자인 토큰 중 하나의 키만 추출
- * @returns ThemeColors
- */
-function transformTokensToThemeColors(tokens: ThemeColors) {
-  const themes: ThemeColors = {};
-
-  Object.entries(tokens).forEach(([themeName, themeTokens]) => {
-    themes[themeName] = {};
-    Object.entries(themeTokens).forEach(([key, value]) => {
-      // key에서 `color-` prefix 제거 및 dash(-)는 underBar(_)로 수정
-      // ex) color-text-content-normal -> text_content_normal
-      const tailwindKey = key.replace(/color-/g, "").replace(/-/g, "_");
-      themes[themeName][tailwindKey] = value;
-    });
-  });
-
-  return themes;
-}
-
-// TODO: semantic만 변환 및 저장 => 추후 primitive에 대한 논의 필요
-const themeColors = transformTokensToThemeColors(designTokens.semantic);
 
 export default {
   content: [
@@ -65,8 +44,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        light: themeColors.light,
-        dark: themeColors.dark,
+        light: designTokens.semantic.light,
+        dark: designTokens.semantic.dark,
       },
     },
   },
