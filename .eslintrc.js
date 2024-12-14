3f04a4b3dde8a60a7581321e1fd8a241cad3d52e
jiyeeeah <sije3757@snu.ac.kr>
2024-12-14 20:18:18 +0900

    feat: ESLint airbnb 설치
    - 일부 라이브러리 이전 버전으로 설치함
      (@typescript-eslint/eslint-plugin, @typescript-eslint/parser)
      - 원인 파악 후 버전 최신으로 수정해야 함

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: "@typescript-eslint/parser", // TypeScript 구문을 파싱할 파서 설정
+  parserOptions: {
+    project: "./tsconfig.json", // TypeScript 프로젝트 설정 파일 경로
+  },
+  extends: [
+    "airbnb", // Airbnb 스타일 규칙 사용
+    "airbnb-typescript", // TypeScript용 Airbnb 규칙 사용
+    "plugin:@typescript-eslint/recommended", // TypeScript에 대한 권장 ESLint 규칙 사용
+    "next/core-web-vitals",
+    "next/typescript",
+    "prettier",
+  ],
+  plugins: [
+    "@typescript-eslint", // TypeScript 관련 ESLint 플러그인
+  ],
+  rules: {
+    // 규칙 추가
+  },
+};
