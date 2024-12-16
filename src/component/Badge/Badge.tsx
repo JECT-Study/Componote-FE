93f0a9cfc90c82ae2204d71fd47616a42fcabc0a
jiyeeeah <sije3757@snu.ac.kr>
2024-12-16 18:58:51 +0900

    [✨feat]: Badge 컴포넌트 props 추가
    - Badge 컴포넌트는 3가지 종류를 가짐
      : dot, new, count
    - 세가지 종류 중 하나를 variant로 갖도록 함

--- src/component/Badge/Badge.tsx
+++ src/component/Badge/Badge.tsx
@@ -2,23 +2,26 @@
  * Avatar 컴포넌트에 필요한 props입니다
  */
 interface BadgeProps {
+  variant: "dot" | "new" | "count";
   text?: string;
 }
 
 /**
  * 프로필 사진을 보여주는 Avatar 컴포넌트입니다
  */
-export default function Badge({ text }: BadgeProps) {
+export default function Badge({ variant, text }: BadgeProps) {
   const basicBadgeClass =
-    "rounded-circle opacity-visible bg-light-feedbackNotification";
+    "rounded-circle opacity-visible bg-light-feedback-notification";
 
-  return text ? (
+  if (variant === "dot") {
+    return <div className={`size-1 ${basicBadgeClass}`} />;
+  }
+
+  return (
     <div
-      className={`${basicBadgeClass} text-light-objectStaticInvHero text-center`}
+      className={`h-5 min-w-5 w-fit ${basicBadgeClass} text-light-object-static-inv-hero text-center typo-body-2xs `}
     >
-      {text}
+      {text || "N"}
     </div>
-  ) : (
-    <div className={`size-1 ${basicBadgeClass}`} />
   );
 }
