97b4493a508a23dee309c3955a6af78e4ee6b99c
jiyeeeah <sije3757@snu.ac.kr>
2024-12-18 16:26:57 +0900

    [🔨chore]: 불필요한 파일 제거 및 폴더명 변경
    - component -> components 로 파일 명 변경
      - tailwind.config.ts에 components로 설정되어 있음

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -38,8 +38,8 @@ const designTokens: DesignTokens = JSON.parse(designTokensString); // 파싱 두
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/stories/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
