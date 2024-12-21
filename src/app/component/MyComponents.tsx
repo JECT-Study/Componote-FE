8f972230a4bf361cf68abffb5baf6312f25dbbb6
ccconac <qwerpoiu91@naver.com>
2024-12-21 15:15:42 +0900

    [♻️refactor]: styled-components 사용 파일 분리 및 불필요한 'use client' 제거

--- src/app/component/MyComponents.tsx
+++ src/app/component/MyComponents.tsx
@@ -1,21 +1,9 @@
-"use client";
-
-import styled from "styled-components";
-
-const Wrapper = styled.div`
-  background-color: #f0f0f0;
-  padding: 20px;
-  border-radius: 8px;
-`;
-
-const Title = styled.h1`
-  color: #333;
-`;
+import * as S from "../../styles/MyComponent.style";
 
 export default function MyComponent() {
   return (
-    <Wrapper>
-      <Title>스타일 컴포넌트 적용 확인</Title>
-    </Wrapper>
+    <S.Wrapper>
+      <S.Title>스타일 컴포넌트 적용 확인</S.Title>
+    </S.Wrapper>
   );
 }
--- src/app/component/page.tsx
+++ src/app/component/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import MyComponent from "./MyComponents";
 
 export default function Component() {
