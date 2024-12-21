91ab5e0599027fe2c5d70d1e75a59a3c24fcecab
jiyeeeah <sije3757@snu.ac.kr>
2024-12-21 23:07:53 +0900

    [📝docs]: theme 사용 예시

--- src/components/MyComponent/MyComponent.style.ts
+++ src/components/MyComponent/MyComponent.style.ts
@@ -3,11 +3,12 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  background-color: #f0f0f0;
+  background-color: ${({ theme }) => theme.colors?.light["feedback-positive"]};
   padding: 20px;
-  border-radius: 8px;
+  border-radius: ${({ theme }) => theme.radius?.["2xs"]};
 `;
 
 export const Title = styled.h1`
   color: #333;
+  ${({ theme }) => theme.typography?.title[3]}
 `;
