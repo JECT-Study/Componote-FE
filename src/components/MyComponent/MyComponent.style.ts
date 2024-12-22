160edcfa040cdd5dc9bc9f85771a378a72bcde07
jiyeeeah <sije3757@snu.ac.kr>
2024-12-22 15:10:08 +0900

    [📝docs]: theme 사용 예시 추가

--- src/components/MyComponent/MyComponent.style.ts
+++ src/components/MyComponent/MyComponent.style.ts
@@ -4,8 +4,12 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.colors?.light["feedback-positive"]};
+  opacity: ${({ theme }) => theme.opacity?.[22]};
   padding: 20px;
+  border-style: solid;
   border-radius: ${({ theme }) => theme.radius?.["2xs"]};
+  border-width: ${({ theme }) => theme.stroke?.boldest};
+  border-color: ${({ theme }) => theme.colors?.light["feedback-negative"]};
 `;
 
 export const Title = styled.h1`
