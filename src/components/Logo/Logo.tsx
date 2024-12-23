6ccd85985002d9d90201451d22bd81338ea1e60f
ccconac <qwerpoiu91@naver.com>
2024-12-23 20:28:10 +0900

    [♻️refactor]: 로고 아이콘 크기 유동적으로 변경할 수 있도록 수정

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -7,18 +7,12 @@ interface ILogo {
 export default function Logo({ isFooter }: ILogo) {
   return (
     <S.LogoContainer>
-      <S.LogoIconImg
-        src="/logo/logo_icon.svg"
-        alt="logo icon"
-        width={16}
-        height={16}
-      />
-      <S.LogoText
-        src={isFooter ? "/logo/logo_white.svg" : "/logo/logo.svg"}
-        alt="logo text"
-        width={53.9}
-        height={14}
-      />
+      {isFooter ? (
+        <S.LogoIconImg width={16} height={16} />
+      ) : (
+        <S.LogoIconImg width={24} height={24} />
+      )}
+      {isFooter ? <S.LogoWhiteTextImg /> : <S.LogoTextImg />}
     </S.LogoContainer>
   );
 }
