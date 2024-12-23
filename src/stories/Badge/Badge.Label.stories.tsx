1e194ece402dedd6b015a06abd336b8f1bb699e7
jiyeeeah <sije3757@snu.ac.kr>
2024-12-23 19:19:20 +0900

    [✨feat]: Badge/Label 컴포넌트 스토리 추가
    - 다양한 경우의 수 별로 스토리 추가
    - 단, size의 경우 너무 많아지기 때문에 toggle로
      변경해볼 수 있도록 함

--- src/stories/Badge/Badge.Label.stories.tsx
+++ src/stories/Badge/Badge.Label.stories.tsx
@@ -1,25 +1,127 @@
-import BadgeLabel from "../../components/Badge/Badge.Label";
+import type { Meta, StoryObj } from "@storybook/react";
+import { BadgeLabel } from "../../components";
 
-export default {
+const meta = {
   component: BadgeLabel,
   title: "Badge/Label",
   tags: ["autodocs"],
   excludeStories: /.*Data$/,
-};
+  argTypes: {
+    $variant: {
+      options: ["labelOnly", "rightIcon"],
+      control: { type: "radio" },
+    },
+    $feedback: {
+      options: ["none", "negative", "info", "positive", "notification"],
+      control: { type: "radio" },
+    },
+    $style: {
+      options: ["solid", "transparent", "outlined"],
+      control: { type: "radio" },
+    },
+    $size: {
+      options: ["xs", "sm", "md"],
+      control: { type: "radio" },
+    },
+  },
+} satisfies Meta<typeof BadgeLabel>;
 
-export const Default = {
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
   args: {
-    variant: "labelOnly",
+    $variant: "labelOnly",
     text: "레이블",
-    feedback: "none",
-    style: "solid",
-    size: "xs",
+    $feedback: "none",
+    $style: "solid",
+    $size: "md",
   },
 };
 
-export const RightIcon = {
+export const IconNoneSolid: Story = {
   args: {
     ...Default.args,
-    variant: "rightIcon",
+    $variant: "rightIcon",
+  },
+};
+
+export const LabelNoneTransparent: Story = {
+  args: {
+    ...Default.args,
+    $style: "transparent",
+  },
+};
+
+export const IconNoneTransparent: Story = {
+  args: {
+    ...Default.args,
+    $style: "transparent",
+    $variant: "rightIcon",
+  },
+};
+
+export const LabelNoneOutlined: Story = {
+  args: {
+    ...Default.args,
+    $style: "outlined",
+  },
+};
+
+export const IconNoneOutlined: Story = {
+  args: {
+    ...Default.args,
+    $style: "outlined",
+    $variant: "rightIcon",
+  },
+};
+
+export const LabelNegativeSolid: Story = {
+  args: {
+    ...Default.args,
+    $feedback: "negative",
+  },
+};
+
+export const IconNegativeSolid: Story = {
+  args: {
+    ...Default.args,
+    $feedback: "negative",
+    $variant: "rightIcon",
+  },
+};
+
+export const LabelNegativeTransparent: Story = {
+  args: {
+    ...Default.args,
+    $feedback: "negative",
+    $style: "transparent",
+  },
+};
+
+export const IconNegativeTransparent: Story = {
+  args: {
+    ...Default.args,
+    $feedback: "negative",
+    $style: "transparent",
+    $variant: "rightIcon",
+  },
+};
+
+export const LabelNegativeOutlined: Story = {
+  args: {
+    ...Default.args,
+    $feedback: "negative",
+    $style: "outlined",
+  },
+};
+
+export const IconNegativeOutlined: Story = {
+  args: {
+    ...Default.args,
+    $feedback: "negative",
+    $style: "outlined",
+    $variant: "rightIcon",
   },
 };
