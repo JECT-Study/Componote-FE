38d8009aad97a93dc346289aa8d92cd11d4e1f93
jiyeeeah <sije3757@snu.ac.kr>
2024-12-23 22:41:30 +0900

    [🔨chore]: 제거된 MyComponent index에서도 제거

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 "use client";
 
-export { default as MyComponent } from "./MyComponent/MyComponent";
 export { default as Divider } from "./Divider/Divider";
 export { default as Logo } from "./Logo/Logo";
 export { default as Footer } from "./Footer/Footer";
