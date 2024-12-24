2e08790a214a7df0a8ae261ad6dcedc657753838
jiyeeeah <sije3757@snu.ac.kr>
2024-12-24 15:11:29 +0900

    [♻️refactor]: Callout 컴포넌트 style 파일 디자인시스템 수정

--- src/components/Callout/Callout.style.tsx
+++ src/components/Callout/Callout.style.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import styled from "styled-components";
+import DESIGN_SYSTEM from "@/styles/designSystem";
 
 /**
  * Callout 컴포넌트에 필요한 style props입니다
@@ -25,34 +26,34 @@ export const CalloutContainer = styled.div<ICalloutStyle>`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  padding: ${({ theme }) => `${theme.gap?.xl} ${theme.gap?.["2xl"]}`};
+  padding: ${DESIGN_SYSTEM.gap.xl} ${DESIGN_SYSTEM.gap["2xl"]};
 
-  padding: ${({ theme }) => theme.gap?.["4xs"]};
+  padding: ${DESIGN_SYSTEM.gap["4xs"]};
 
-  border-radius: ${({ theme }) => theme.radius?.xs};
+  border-radius: ${DESIGN_SYSTEM.radius.xs};
   border: ${({ theme }) =>
-    `${theme.stroke?.normal} solid ${theme.colors?.light["border-trans-normal"]}`};
-  opacity: ${({ theme }) => theme.opacity?.visible};
-  background: ${({ theme }) => theme.colors?.light["surface-raised"]};
+    `${DESIGN_SYSTEM.stroke.normal} solid ${theme.light["border-trans-normal"]}`};
+  opacity: ${DESIGN_SYSTEM.opacity.visible};
+  background: ${({ theme }) => theme.light["surface-raised"]};
 `;
 
 export const CalloutTitleContainer = styled.div`
   display: flex;
   align-items: flex-start;
   align-self: stretch;
-  gap: ${({ theme }) => theme.gap?.xs};
-  padding: ${({ theme }) => theme.gap?.none};
+  gap: ${DESIGN_SYSTEM.gap.xs};
+  padding: ${DESIGN_SYSTEM.gap.none};
 
-  border-radius: ${({ theme }) => theme.radius?.none};
-  opacity: ${({ theme }) => theme.opacity?.visible};
+  border-radius: ${DESIGN_SYSTEM.radius.none};
+  opacity: ${DESIGN_SYSTEM.opacity.visible};
 `;
 
 export const CalloutTitleText = styled.span`
   max-height: 3.5rem
   flex: 1 0 0;
 
-  ${({ theme }) => theme.typo?.title[1]};
-  color: ${({ theme }) => theme.colors?.light["object-hero"]};
+  ${DESIGN_SYSTEM.typography.title[1]};
+  color: ${({ theme }) => theme.light["object-hero"]};
 `;
 
 export const CalloutBodyText = styled.div`
@@ -60,8 +61,8 @@ export const CalloutBodyText = styled.div`
   align-self: stretch;
   overflow: hidden;
 
-  ${({ theme }) => theme.typo?.label.md};
-  color: ${({ theme }) => theme.colors?.light["object-subtle"]};
+  ${DESIGN_SYSTEM.typography.label.md};
+  color: ${({ theme }) => theme.light["object-subtle"]};
   text-overflow: ellipsis;
   white-space: nowrap;
 `;
