d0a3686baaa53720dac59348749934b7a5ad6db8
jiyeeeah <sije3757@snu.ac.kr>
2024-12-24 15:36:46 +0900

    [🎨style]: Callout 컴포넌트 사이즈에 맞춘 style 적용

--- src/components/Callout/Callout.style.tsx
+++ src/components/Callout/Callout.style.tsx
@@ -7,17 +7,33 @@ import DESIGN_SYSTEM from "@/styles/designSystem";
  * Callout 컴포넌트에 필요한 style props입니다
  */
 export interface ICalloutStyle {
-  $size: "sm" | "md";
+  $size: keyof typeof sizeMap;
 }
 
-// TODO: size에 따라 text, padding 값 등 조절
-// const sizeMap = {
-//   sm: {
-//     titleTypo: (theme: DefaultTheme) => theme.typo?.label.bold.lg,
-//     bodyTypo: (theme: DefaultTheme) => theme.typo?.label.xs,
-//   },
-//   md: {},
-// };
+const sizeMap = {
+  sm: {
+    containerPadding: `${DESIGN_SYSTEM.gap.lg} ${DESIGN_SYSTEM.gap.xl}`,
+    titleText: `
+    ${DESIGN_SYSTEM.typography.label.bold.lg};
+    max-height: 3.375rem;
+  `,
+    bodyText: `
+    ${DESIGN_SYSTEM.typography.label.xs};
+    height: 2.625rem;
+  `,
+  },
+  md: {
+    containerPadding: `${DESIGN_SYSTEM.gap.xl} ${DESIGN_SYSTEM.gap["2xl"]}`,
+    titleText: `
+    ${DESIGN_SYSTEM.typography.title[1]};
+    max-height: 3.5rem;
+  `,
+    bodyText: `
+    ${DESIGN_SYSTEM.typography.label.md};
+    height: 3rem;
+  `,
+  },
+};
 
 export const CalloutContainer = styled.div<ICalloutStyle>`
   width: 30rem;
@@ -26,9 +42,9 @@ export const CalloutContainer = styled.div<ICalloutStyle>`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  padding: ${DESIGN_SYSTEM.gap.xl} ${DESIGN_SYSTEM.gap["2xl"]};
+  gap: ${DESIGN_SYSTEM.gap["3xs"]};
 
-  padding: ${DESIGN_SYSTEM.gap["4xs"]};
+  padding: ${(props) => sizeMap[props.$size].containerPadding};
 
   border-radius: ${DESIGN_SYSTEM.radius.xs};
   border: ${({ theme }) =>
@@ -48,20 +64,19 @@ export const CalloutTitleContainer = styled.div`
   opacity: ${DESIGN_SYSTEM.opacity.visible};
 `;
 
-export const CalloutTitleText = styled.span`
-  max-height: 3.5rem
+export const CalloutTitleText = styled.span<ICalloutStyle>`
+  ${(props) => sizeMap[props.$size].titleText}
   flex: 1 0 0;
 
-  ${DESIGN_SYSTEM.typography.title[1]};
   color: ${({ theme }) => theme.light["object-hero"]};
 `;
 
-export const CalloutBodyText = styled.div`
-  height: 3rem;
+export const CalloutBodyText = styled.div<ICalloutStyle>`
+  ${(props) => sizeMap[props.$size].bodyText}
+
   align-self: stretch;
   overflow: hidden;
 
-  ${DESIGN_SYSTEM.typography.label.md};
   color: ${({ theme }) => theme.light["object-subtle"]};
   text-overflow: ellipsis;
   white-space: nowrap;
--- src/components/Callout/Callout.tsx
+++ src/components/Callout/Callout.tsx
@@ -1,7 +1,7 @@
 import * as S from "./Callout.style";
 
 /**
- * Badge 컴포넌트에 필요한 props입니다
+ * Callout 컴포넌트에 필요한 props입니다
  */
 interface ICallout extends S.ICalloutStyle {
   titleText: string;
@@ -9,16 +9,16 @@ interface ICallout extends S.ICalloutStyle {
 }
 
 /**
- * Badge 컴포넌트입니다
+ * Callout 컴포넌트입니다
  */
 export default function Badge({ $size, titleText, bodyText }: ICallout) {
   return (
     <S.CalloutContainer $size={$size}>
       <S.CalloutTitleContainer>
-        <S.CalloutTitleText>{titleText}</S.CalloutTitleText>
+        <S.CalloutTitleText $size={$size}>{titleText}</S.CalloutTitleText>
         {/* TODO: 추후 버튼 컴포넌트가 만들어지고 나면 button/empty/secondary 넣어줘야 함 */}
       </S.CalloutTitleContainer>
-      <S.CalloutBodyText>{bodyText}</S.CalloutBodyText>
+      <S.CalloutBodyText $size={$size}>{bodyText}</S.CalloutBodyText>
     </S.CalloutContainer>
   );
 }
