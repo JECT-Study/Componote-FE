cdc7baf9c6e7a8089170d39503486d2041c7154f
ccconac <qwerpoiu91@naver.com>
2024-12-26 16:00:51 +0900

    [🔥remove]: component page에서 chip component 제거

--- src/app/component/page.tsx
+++ src/app/component/page.tsx
@@ -1,20 +1,3 @@
-"use client";
-
-import { Chip } from "@/components";
-import GitHubIcon from "../../assets/icons/github-fill.svg";
-
 export default function Component() {
-  return (
-    <div>
-      <Chip
-        text="레이블"
-        $size="lg"
-        $isSelected
-        $iconVisible
-        $isInversed
-        $isDisabled={false}
-        IconComponent={GitHubIcon}
-      />
-    </div>
-  );
+  return <div></div>;
 }
