7ff1a263cda44cd82e5b93c21a2fee6acf01a02d
jiyeeeah <sije3757@snu.ac.kr>
2024-12-27 14:50:03 +0900

    [♻️refactor]: Callout 컴포넌트 types와 theme 파일 분리

---
+++ src/components/Callout/Callout.theme.ts
@@ -0,0 +1,55 @@
+import DESIGN_SYSTEM from "@/styles/designSystem";
+
+export const CALLOUT_SIZE = {
+  sm: {
+    containerPadding: `${DESIGN_SYSTEM.gap.lg} ${DESIGN_SYSTEM.gap.xl}`,
+    titleText: `
+    ${DESIGN_SYSTEM.typography.label.bold.lg};
+    max-height: 3.375rem;
+  `,
+    bodyText: `
+    ${DESIGN_SYSTEM.typography.label.xs};
+    height: 2.625rem;
+  `,
+  },
+  md: {
+    containerPadding: `${DESIGN_SYSTEM.gap.xl} ${DESIGN_SYSTEM.gap["2xl"]}`,
+    titleText: `
+    ${DESIGN_SYSTEM.typography.title[1]};
+    max-height: 3.5rem;
+  `,
+    bodyText: `
+    ${DESIGN_SYSTEM.typography.label.md};
+    height: 3rem;
+  `,
+  },
+};
+
+export const CALLOUT_INTERACTIVE_SIZE = {
+  sm: {
+    containerPadding: `${DESIGN_SYSTEM.gap.lg} ${DESIGN_SYSTEM.gap.xl}`,
+    iconSize: DESIGN_SYSTEM.iconSize.lg,
+    titleText: `
+    ${DESIGN_SYSTEM.typography.label.bold.lg};
+    height: 1.6875rem;
+    max-height: 3.375rem;
+  `,
+    bodyText: `
+    ${DESIGN_SYSTEM.typography.label.xs};
+    height: 2.625rem;
+  `,
+  },
+  md: {
+    containerPadding: `${DESIGN_SYSTEM.gap.xl} ${DESIGN_SYSTEM.gap["2xl"]}`,
+    iconSize: DESIGN_SYSTEM.iconSize.xl,
+    titleText: `
+    ${DESIGN_SYSTEM.typography.title[1]};
+    height: 1.75rem;
+    max-height: 3.5rem;
+  `,
+    bodyText: `
+    ${DESIGN_SYSTEM.typography.label.md};
+    height: 3rem;
+  `,
+  },
+};
---
+++ src/components/Callout/Callout.types.ts
@@ -0,0 +1,32 @@
+import { CALLOUT_INTERACTIVE_SIZE, CALLOUT_SIZE } from "./Callout.theme";
+
+/**
+ * Callout 컴포넌트에 필요한 style props입니다
+ */
+export interface ICalloutStyle {
+  $size: keyof typeof CALLOUT_SIZE;
+}
+
+/**
+ * Callout 컴포넌트에 필요한 props입니다
+ */
+export interface ICallout extends ICalloutStyle {
+  titleText: string;
+  bodyText?: string;
+}
+
+/**
+ * Callout/Interactive 컴포넌트에 필요한 style props입니다
+ */
+export interface ICalloutInteractiveStyle {
+  $size: keyof typeof CALLOUT_INTERACTIVE_SIZE;
+  $disabled?: boolean;
+}
+
+/**
+ * Callout/Interactive 컴포넌트에 필요한 props입니다
+ */
+export interface ICalloutInteractive extends ICalloutInteractiveStyle {
+  titleText: string;
+  bodyText?: string;
+}
