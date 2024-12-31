b23f5722e25be19aa9a11cf078c53bf3be77428b
ccconac <qwerpoiu91@naver.com>
2025-01-01 01:36:44 +0900

    [🔥remove]: 불필요한 import 구문 삭제

--- src/components/InputField/InputField.style.tsx
+++ src/components/InputField/InputField.style.tsx
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import DESIGN_SYSTEM from "@/styles/designSystem";
 import { IIconWrapper, IInputComponent } from "./InputField.types";
 
