63a44e870aa23e44a83ec4776bff34d52b63d8a1
jiyeeeah <sije3757@snu.ac.kr>
2025-01-01 19:58:34 +0900

    [♻️refactor]: Interaction 컴포넌트의 variant를 enum화

---
+++ src/components/Interaction/Interaction.types.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/prefer-default-export
+export const enum InteractionVariant {
+  DEFAULT = "default",
+  ACCENT = "accent",
+  INFO = "info",
+  NEGATIVE = "negative",
+}
