1d051b7333ca2101f565576f9295ba1117c3c95b
ccconac <qwerpoiu91@naver.com>
2025-01-05 17:14:54 +0900

    [🔥remove]: input field component 예시 삭제

--- src/app/component/page.tsx
+++ src/app/component/page.tsx
@@ -1,22 +1,3 @@
-"use client";
-
-import { InputField } from "@/components";
-import BlankLine from "@/assets/icons/blank-line.svg";
-
 export default function Component() {
-  return (
-    <div>
-      <InputField
-        labelText="필드 레이블"
-        helperText="헬퍼 메시지"
-        placeholderText="플레이스 홀더"
-        $size="md"
-        $icon={BlankLine}
-        $isNagative={false}
-        $iconVisible
-        $labelVisible
-        $helperVisible
-      />
-    </div>
-  );
+  return <div>component</div>;
 }
