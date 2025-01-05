b87c715690f3bc749eba6ee60fe000e429dfaa0e
ccconac <qwerpoiu91@naver.com>
2025-01-05 17:52:27 +0900

    [✨feat]: navigationBar component 구현

--- src/components/NavigationBar/NavigationBar.types.ts
+++ src/components/NavigationBar/NavigationBar.types.ts
@@ -2,3 +2,12 @@ export interface INavItem {
   text: string;
   href: string;
 }
+
+export interface INavigation {
+  $isSeparated: boolean;
+  $isAuthorized: boolean;
+}
+
+export interface IInputField {
+  placeholderText: string;
+}
