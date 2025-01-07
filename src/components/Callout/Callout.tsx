332a8a1282aad16921a7b7cbf8781794f4af2a57
ccconac <qwerpoiu91@naver.com>
2025-01-07 14:33:30 +0900

    [✨feat]: callout component에 button component 추가

--- src/components/Callout/Callout.tsx
+++ src/components/Callout/Callout.tsx
@@ -1,15 +1,31 @@
+import { Button } from "@/components";
+import { ButtonStyle } from "../Button/Button.types";
 import * as S from "./Callout.style";
 import { ICallout } from "./Callout.types";
+import rightIcon from "@/assets/icons/chevron-right-line.svg";
 
 /**
  * Callout 컴포넌트입니다
  */
-export default function Callout({ $size, titleText, bodyText }: ICallout) {
+export default function Callout({
+  $size,
+  titleText,
+  bodyText,
+  buttonLabelText,
+}: ICallout) {
   return (
     <S.CalloutContainer $size={$size}>
       <S.CalloutTitleContainer>
         <S.CalloutTitleText $size={$size}>{titleText}</S.CalloutTitleText>
-        {/* TODO: 추후 버튼 컴포넌트가 만들어지고 나면 button/empty/secondary 넣어줘야 함 */}
+        <Button
+          $isLeftIconVisible
+          $isRightIconVisible={false}
+          $leftIcon={rightIcon}
+          text={buttonLabelText}
+          $size="sm"
+          $buttonType="iconButton"
+          $buttonStyle={ButtonStyle.EmptySecondary}
+        />
       </S.CalloutTitleContainer>
       {bodyText && (
         <S.CalloutBodyText $size={$size}>{bodyText}</S.CalloutBodyText>
