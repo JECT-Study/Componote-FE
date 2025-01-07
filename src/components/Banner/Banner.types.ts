4853251c6586d5814da1e56291480f3ed67b4a14
ccconac <qwerpoiu91@naver.com>
2025-01-07 14:35:08 +0900

    [♻️refactor]: default banner props 타입 파일 분리

--- src/components/Banner/Banner.types.ts
+++ src/components/Banner/Banner.types.ts
@@ -1,4 +1,12 @@
 /**
+ * Banner variant @component @announcement @designSystem @bookmark에 필요한 props입니다
+ */
+export interface IDefaultBanner {
+  titleText: string;
+  descriptionText: string;
+}
+
+/**
  * Banner variant @onboarding에 필요한 props입니다
  */
 export interface IOnboardingBanner {
