6dba039bf3e2ed2eaec7d5b0ca9bbb5961ab7e98
ccconac <qwerpoiu91@naver.com>
2025-01-15 03:03:36 +0900

    [✨feat]: component card component 구현

---
+++ src/components/Card/Card.component.tsx
@@ -0,0 +1,44 @@
+import { BadgeComponentType, Button } from "@/components";
+import boxIcon from "@/assets/icons/box-fill.svg";
+import commentIcon from "@/assets/icons/chat-line.svg";
+import bookmarkIcon from "@/assets/icons/bookmark-line.svg";
+
+import CardItem from "./CardItem";
+import * as S from "./Card.component.style";
+import { ButtonStyle } from "../Button/Button.types";
+import { IComponentCard, IComponentCardComponent } from "./Card.types";
+
+export default function ComponentCard({
+  $src,
+  componentName,
+  descriptionText,
+  $sampleCount,
+  $commentCount,
+  $bookmarkCount,
+}: IComponentCard & IComponentCardComponent) {
+  return (
+    <S.CardContainer>
+      <S.ImageBox>
+        <S.CardImage $src={$src} />
+        <BadgeComponentType $size="xs" $type="input" $style="solid" />
+      </S.ImageBox>
+      <S.DescriptionBox>
+        <S.TitleText>{componentName}</S.TitleText>
+        <S.BodyText>{descriptionText}</S.BodyText>
+      </S.DescriptionBox>
+      <S.ContentSection>
+        <S.ContentContainer>
+          <CardItem $icon={boxIcon} text={$sampleCount} />
+          <CardItem $icon={commentIcon} text={$commentCount} />
+        </S.ContentContainer>
+        <Button
+          text={$bookmarkCount}
+          $size="sm"
+          $buttonType="button"
+          $leftIcon={bookmarkIcon}
+          $buttonStyle={ButtonStyle.OutlinedSecondary}
+        />
+      </S.ContentSection>
+    </S.CardContainer>
+  );
+}
--- src/components/Card/Card.types.ts
+++ src/components/Card/Card.types.ts
@@ -1,3 +1,16 @@
+export interface IComponentCardComponent {
+  $src?: string;
+}
+
+/* component card에 사용되는 props type입니다. */
+export interface IComponentCard {
+  componentName: string;
+  descriptionText: string;
+  $sampleCount: string;
+  $commentCount: string;
+  $bookmarkCount: string;
+}
+
 /* card item component에 사용되는 props type입니다. */
 export interface ICardItem {
   $icon: React.ElementType;
