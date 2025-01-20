12d283ec9f88cb1a68288f764be1169d02a5403e
ccconac <qwerpoiu91@naver.com>
2025-01-20 17:29:05 +0900

    [✨feat]: component page UI 구현

--- src/app/component/page.tsx
+++ src/app/component/page.tsx
@@ -1,3 +1,68 @@
+"use client";
+
+import {
+  Layout,
+  NavigationBar,
+  DefaultBanner,
+  Toolbar,
+  ChipList,
+  ComponentCard,
+  Footer,
+} from "@/components";
+import { BANNER_TEXT } from "@/constants/messages";
+import CardContainer from "./_components/CardContainer";
+
 export default function Component() {
-  return <div>component</div>;
+  return (
+    <Layout>
+      <NavigationBar
+        $isAuthorized
+        $isSeparated
+        placeholderText="컴포넌트나 디자인 시스템을 검색해 보세요..."
+      />
+      <DefaultBanner
+        titleText={BANNER_TEXT.component.titleText}
+        descriptionText={BANNER_TEXT.component.descriptionText}
+      />
+      <Toolbar children={<ChipList />} />
+      <CardContainer>
+        <ComponentCard
+          componentName="컴포넌트 명"
+          descriptionText="설명 내용"
+          $sampleCount="999+"
+          $commentCount="999+"
+          $bookmarkCount="999+"
+        />
+        <ComponentCard
+          componentName="컴포넌트 명"
+          descriptionText="설명 내용"
+          $sampleCount="999+"
+          $commentCount="999+"
+          $bookmarkCount="999+"
+        />
+        <ComponentCard
+          componentName="컴포넌트 명"
+          descriptionText="설명 내용"
+          $sampleCount="999+"
+          $commentCount="999+"
+          $bookmarkCount="999+"
+        />
+        <ComponentCard
+          componentName="컴포넌트 명"
+          descriptionText="설명 내용"
+          $sampleCount="999+"
+          $commentCount="999+"
+          $bookmarkCount="999+"
+        />
+        <ComponentCard
+          componentName="컴포넌트 명"
+          descriptionText="설명 내용"
+          $sampleCount="999+"
+          $commentCount="999+"
+          $bookmarkCount="999+"
+        />
+      </CardContainer>
+      <Footer />
+    </Layout>
+  );
 }
