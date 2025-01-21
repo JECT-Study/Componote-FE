aa2429e08aec33f5e94b1479e73b82179b0cf3ce
ccconac <qwerpoiu91@naver.com>
2025-01-21 14:59:50 +0900

    [✨feat]: logo 클릭 시 온보딩 페이지로 redirect

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,8 +1,11 @@
 import * as S from "./Logo.style";
+import { useRouter } from "next/navigation";
 
 export default function Logo({ $isFooter = false }: S.ILogo) {
+  const router = useRouter();
+
   return (
-    <S.LogoContainer>
+    <S.LogoContainer onClick={() => router.push("/")}>
       <S.LogoIconImg $isFooter={$isFooter} />
       <S.LogoTextImg $isFooter={$isFooter} />
     </S.LogoContainer>
