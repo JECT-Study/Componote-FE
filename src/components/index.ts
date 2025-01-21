0f290245f49ed10844cc18ab12ad8f1af2597fb1
ccconac <qwerpoiu91@naver.com>
2025-01-21 19:18:38 +0900

    [🚚move]: 특정 page에서만 사용되는 component 디렉터리 구조 변경

--- src/components/index.ts
+++ src/components/index.ts
@@ -32,5 +32,5 @@ export { default as ComponentCard } from "./Card/Card.component";
 export { default as DesignSystemCard } from "./Card/Card.designSystem";
 export { default as Dialog } from "./Dialog/Dialog";
 export { default as Layout } from "./Layout/Layout";
-export { default as CardContainer } from "./Container/Component/CardContainer";
-export { default as ImageContainer } from "./Container/Onboarding/ImageContainer";
+export { default as CardContainer } from "./Pages/Component/CardContainer";
+export { default as ImageContainer } from "./Pages/Onboarding/ImageContainer";
