0a407d530dd8a318366b25228fd5576346b373be
이지예 <jessie@ijiyeui-MacBookPro-2.local>
2025-01-23 14:48:55 +0900

    [✨feat]: 랜딩페이지에서 code 받아와 로그인까지
    - 로그인이 안되면 회원가입 페이지로 이동하도록 함
    - 현재는 랜딩페이지에 있지만 "use client"를 쓰기가 좀 그래서
      구조 부분은 더 고민이 필요할 듯

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Footer,
   NavigationBar,
@@ -5,8 +7,39 @@ import {
   OnboardingBanner,
   ImageContainer,
 } from "@/components";
+import { useLoginMutation } from "@/hooks/api/useLoginMutation";
+import { useSocialLoginQuery } from "@/hooks/api/useSocialLoginQuery";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [code, setCode] = useState<string | null>(null);
+
+  useEffect(() => {
+    // URL에서 code 파라미터 가져오기
+    const codeParam = searchParams.get("code");
+    if (codeParam) {
+      setCode(codeParam);
+    }
+  }, [searchParams]);
+
+  // TODO : 소셜 로그인 어떤 소셜인지 넣어주기! (현재 임시로 naver)
+  const { socialLoginData } = useSocialLoginQuery("naver", code);
+  const { mutate: login } = useLoginMutation();
+
+  useEffect(() => {
+    if (!code) return;
+    if (!socialLoginData) return;
+    if (!socialLoginData.isRegister) {
+      router.push("/login/signin");
+      return;
+    }
+    login({ socialAccountId: socialLoginData.socialAccountId });
+    router.push("/");
+  }, [code, socialLoginData, login, router]);
+
   return (
     <Layout>
       <NavigationBar
