01ee4a229b33e943b57eff7388ada35a84fa7ec1
ccconac <qwerpoiu91@naver.com>
2025-02-03 12:50:40 +0900

    [✨feat]: empty state component 구현

---
+++ src/components/EmptyState/EmptyState.style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import DESIGN_SYSTEM from "@/styles/designSystem";
+
+export const EmptyStateContainer = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  min-height: 15rem;
+
+  gap: ${DESIGN_SYSTEM.gap["3xs"]};
+  padding: ${DESIGN_SYSTEM.gap["4xl"]} ${DESIGN_SYSTEM.gap["7xl"]};
+`;
+
+export const EmptyStateText = styled.span`
+  text-align: center;
+  color: ${({ theme }) => theme.light["object-subtler"]};
+
+  ${DESIGN_SYSTEM.typography.title[2]};
+`;
