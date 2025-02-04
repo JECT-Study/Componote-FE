2fd228cdae02b92c27cea42604ec06a4d4804c64
Jiye Lee <sije3757@snu.ac.kr>
2025-02-05 07:48:43 +0900

    [♻️refactor]: Login 로직 구현 및 관련 상태 관리 (#62)
    * [♻️refactor]: socialLogin 상태관리
    
    - useSocialLoginStore 만들어, socialLogin의 provider와 인가코드를 관리
    
    - 리다이렉트 되는 과정에서 socialLoginState가 초기화되는 문제가 있었음
      -> 해결하기 위해 zustand의 persist활용 및 localStorage에 state 저장
    
    * [♻️refactor]: socialLogin provider Enum화
    
    * [✨feat]: AccessToken 관리하는 useTokenStore 구현
    
    * [🔨chore]: 불필요한 파일 제거
    
    - 예시를 위해 제작했언 useLetterStore 제거함
    
    * [✨feat]: 유저 정보 가져오는 api 구현
    
    * [✨feat]: userInfo 상태 구현
    
    - useUserInfoStore를 통해 userInfo 관련 상태를 관리함
    - userTokenStore에는 memberId도 갖도록 수정함
      - useUserInfoQuery에 전달해주기 위해
    
    ---------
    
    Co-authored-by: jiyeeeah <sije3757@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,39 +9,63 @@ import {
 } from "@/components";
 import { useLoginMutation } from "@/hooks/api/useLoginMutation";
 import { useSocialLoginQuery } from "@/hooks/api/useSocialLoginQuery";
-import { useLetterStore } from "@/hooks/store/useLetterStore";
+import { useUserInfoQuery } from "@/hooks/api/useUserInfoQuery";
+import { useSocialLoginStore } from "@/hooks/store/useSocialLoginStore";
+import { useTokenStore } from "@/hooks/store/useTokenStore";
+import { useUserInfoStore } from "@/hooks/store/useUserInfoStore";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const [code, setCode] = useState<string | null>(null);
-  // 예시
-  const { letters } = useLetterStore();
+
+  const { socialLoginState, setAuthCode } = useSocialLoginStore();
+  const { accessToken, memberId, setAccessToken, setMemberId } =
+    useTokenStore();
+  const { userInfo, setUserInfo } = useUserInfoStore();
 
   useEffect(() => {
     // URL에서 code 파라미터 가져오기
     const codeParam = searchParams.get("code");
     if (codeParam) {
-      setCode(codeParam);
+      setAuthCode(codeParam);
     }
-  }, [searchParams]);
+  }, [searchParams, setAuthCode]);
 
-  // TODO : 소셜 로그인 어떤 소셜인지 넣어주기! (현재 임시로 naver)
-  const { socialLoginData } = useSocialLoginQuery("naver", code);
-  const { mutate: login } = useLoginMutation();
+  const { socialLoginData } = useSocialLoginQuery(
+    socialLoginState.provider || "",
+    socialLoginState.authCode
+  );
+  const { mutate: login, data: loginData } = useLoginMutation();
 
   useEffect(() => {
-    if (!code) return;
+    if (!socialLoginState.authCode) return;
     if (!socialLoginData) return;
+
     if (!socialLoginData.isRegister) {
       router.push("/login/signin");
       return;
     }
+
     login({ socialAccountId: socialLoginData.socialAccountId });
     router.push("/");
-  }, [code, socialLoginData, login, router]);
+  }, [socialLoginState.authCode, socialLoginData, login, router]);
+
+  useEffect(() => {
+    if (loginData) {
+      setAccessToken(loginData.accessToken);
+      setMemberId(loginData.memberId);
+    }
+  }, [accessToken, loginData, setAccessToken, setMemberId]);
+
+  const { userInfoData } = useUserInfoQuery(accessToken, memberId);
+
+  useEffect(() => {
+    if (userInfoData) {
+      setUserInfo(userInfoData);
+    }
+  }, [userInfoData, setUserInfo]);
 
   return (
     <Layout>
@@ -50,15 +74,6 @@ export default function Home() {
         $isAuthorized={false}
         placeholderText="플레이스 홀더"
       />
-      {/* 예시 */}
-      <div>
-        {letters.map((letter) => (
-          <div key={letter.id}>
-            <h2>{letter.title}</h2>
-            <p>{letter.content}</p>
-          </div>
-        ))}
-      </div>
       <OnboardingBanner />
       <ImageContainer />
       <Footer />
---
+++ src/hooks/store/useUserInfoStore.ts
@@ -0,0 +1,26 @@
+import { IMemberSummary } from "@/types/user";
+import { create } from "zustand";
+
+interface IUserInfoState {
+  userInfo: IMemberSummary;
+}
+
+interface IUserInfoActions {
+  setUserInfo: (userInfo: IMemberSummary) => void;
+  logout: () => void;
+}
+
+const defaultState: IMemberSummary = {
+  isEmailRegistered: false,
+  nickname: "",
+  profileImageUrl: "",
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export const useUserInfoStore = create<IUserInfoState & IUserInfoActions>(
+  (set) => ({
+    userInfo: defaultState,
+    setUserInfo: (userInfo: IMemberSummary) => set({ userInfo }),
+    logout: () => set({ userInfo: defaultState }),
+  })
+);
