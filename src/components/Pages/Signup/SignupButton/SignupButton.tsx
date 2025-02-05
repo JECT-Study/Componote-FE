cee93eecd2bc222aeea9d43c740c403134401037
jiyeeeah <sije3757@gmail.com>
2025-02-05 15:21:39 +0900

    [✨feat]: 취소하기 버튼에 로직 구현
    - 취소하기 버튼 누르면, useSignupUserStore 초기화 및 홈 페이지로 이동

--- src/components/Pages/Signup/SignupButton/SignupButton.tsx
+++ src/components/Pages/Signup/SignupButton/SignupButton.tsx
@@ -3,12 +3,19 @@ import { ButtonStyle } from "@/components/Button/Button.types";
 import { SIGNUP_TEXT } from "@/constants/messages";
 import { useSignupUserStore } from "@/hooks/store/useSignupUserStore";
 import SignupJobs from "@/types/enum/signupJobs";
+import { useRouter } from "next/navigation";
 import * as S from "./SignupButton.style";
 
 export default function SignupButton() {
-  const { nickname, job, socialAccountId } = useSignupUserStore();
+  const router = useRouter();
+  const { nickname, job, socialAccountId, cancelSignup } = useSignupUserStore();
   const isSubmitDisabled =
     !nickname || job === SignupJobs.NONE || !socialAccountId;
+
+  const handleCancelClick = () => {
+    cancelSignup();
+    router.push("/");
+  };
 
   return (
     <S.SignupButtonContainer>
@@ -16,6 +23,7 @@ export default function SignupButton() {
         text={SIGNUP_TEXT.cancelButtonText}
         $size="md"
         $buttonStyle={ButtonStyle.OutlinedSecondary}
+        onClick={handleCancelClick}
       />
       <Button
         text={SIGNUP_TEXT.submitButtonText}
--- src/hooks/store/useSignupUserStore.ts
+++ src/hooks/store/useSignupUserStore.ts
@@ -8,16 +8,23 @@ interface ISignupUserActions {
   setNickname: (nickName: string) => void;
   setJob: (job: SignupJobs) => void;
   setSocialAccountId: (id: number) => void;
+  cancelSignup: () => void;
 }
+
+const DefaultState = {
+  nickname: "",
+  job: SignupJobs.NONE,
+  socialAccountId: 0,
+};
 
 // eslint-disable-next-line import/prefer-default-export
 export const useSignupUserStore = create<ISignupUserState & ISignupUserActions>(
   (set) => ({
-    nickname: "",
-    job: SignupJobs.NONE,
-    socialAccountId: 0,
+    ...DefaultState,
+
     setNickname: (nickname: string) => set({ nickname }),
     setJob: (job: SignupJobs) => set({ job }),
     setSocialAccountId: (socialAccountId: number) => set({ socialAccountId }),
+    cancelSignup: () => set({ ...DefaultState }),
   })
 );
