a262b6b8deed7e8564e18d130729309102bb2553
jiyeeeah <sije3757@gmail.com>
2025-02-05 10:10:52 +0900

    [🐛fix]: Avatar storybook에 props 잘못 설정되어 있는 오류 수정
    - Vercel Deploy를 통해 발견함

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -5,7 +5,7 @@ import IAvatar from "./Avatar.types";
 /**
  * 프로필 사진을 보여주는 Avatar 컴포넌트입니다
  */
-export default function Avatar({ $badgeVisible, $src, $size }: IAvatar) {
+export default function Avatar({ $badgeVisible = true, $src, $size }: IAvatar) {
   return (
     <S.AvatarContainer $size={$size} $badgeVisible={$badgeVisible}>
       <S.ImageContainer>
