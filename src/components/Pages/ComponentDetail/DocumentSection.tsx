83374ec2dac25d4d3ae5ef3a8f28234a5cbeba55
ccconac <qwerpoiu91@naver.com>
2025-02-05 15:54:52 +0900

    [♻️refactor]: document section 메시지 상수화

--- src/components/Pages/ComponentDetail/DocumentSection.tsx
+++ src/components/Pages/ComponentDetail/DocumentSection.tsx
@@ -2,6 +2,7 @@ import Divider from "@/components/Divider/Divider";
 import * as S from "./DocumentSection.style";
 import { Callout } from "@/components";
 import { IDocumentSection } from "./DocumentSection.types";
+import { COMPONENT_DETAIL_PAGE_TEXT } from "@/constants/messages";
 
 export default function DocumentSection({
   descriptionText,
@@ -12,24 +13,28 @@ export default function DocumentSection({
     <S.DocumentSectionContainer>
       <S.DocumentSection>
         <S.DocumentArticle>
-          <S.TitleText>설명</S.TitleText>
+          <S.TitleText>
+            {COMPONENT_DETAIL_PAGE_TEXT.title.description}
+          </S.TitleText>
           <S.BodyText>{descriptionText}</S.BodyText>
         </S.DocumentArticle>
         <Divider $stroke="normal" $layout="horizontal" />
         <S.DocumentArticle>
-          <S.TitleText>간단 용례</S.TitleText>
+          <S.TitleText>{COMPONENT_DETAIL_PAGE_TEXT.title.useCase}</S.TitleText>
           <S.BodyText>{useCaseText}</S.BodyText>
         </S.DocumentArticle>
         <Divider $stroke="normal" $layout="horizontal" />
         <Callout
           $size="md"
           highlightedText={designReferenceCount}
-          titleText="개의 디자인 시스템에서 이 컴포넌트를 사용하고 있어요"
-          buttonLabelText="보러 가기"
+          titleText={COMPONENT_DETAIL_PAGE_TEXT.callout.title}
+          buttonLabelText={COMPONENT_DETAIL_PAGE_TEXT.callout.buttonLabel}
         />
         <S.DocumentArticle>
-          <S.TitleText>참고자료 및 문헌</S.TitleText>
-          <S.BodyText>컴포넌트에 대한 참고자료 및 문헌</S.BodyText>
+          <S.TitleText>
+            {COMPONENT_DETAIL_PAGE_TEXT.title.reference}
+          </S.TitleText>
+          <S.BodyText>{COMPONENT_DETAIL_PAGE_TEXT.body.reference}</S.BodyText>
         </S.DocumentArticle>
       </S.DocumentSection>
     </S.DocumentSectionContainer>
