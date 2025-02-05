c94aa088f75c629bf02ea11afbd1c67ce9e447e6
ccconac <qwerpoiu91@naver.com>
2025-02-05 23:10:25 +0900

    [✨feat]: component api 구현

---
+++ src/api/component.ts
@@ -0,0 +1,17 @@
+import END_POINT from "@/constants/api";
+import axiosInstance from "./interceptor";
+
+// eslint-disable-next-line import/prefer-default-export
+export const searchComponent = async (
+  page: number,
+  size: number,
+  keyword?: string,
+  types?: string,
+  sort?: string,
+) => {
+  const { data } = await axiosInstance.get(
+    `${END_POINT.searchComponent(page, size, keyword, types, sort)}`,
+  );
+
+  return data;
+};
