a214a8aabf5ef8e5a738b0cf69d248016a21219f
jiyeeeah <sije3757@gmail.com>
2025-02-05 21:37:31 +0900

    [✨feat]: 로그인시 홈 페이지의 NavBar에 프로필 이미지
    - 로그인 상태일 경우에는 NavBar에 로그인 버튼 대신 프로필 이미지 표시

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
+"use client";
+
 import { Footer, NavigationBar, Layout } from "@/components";
 import { MainContent } from "@/components/Pages";
+import { useTokenStore } from "@/hooks/store/useTokenStore";
 import { Suspense } from "react";
 
 export default function Home() {
+  const { accessToken } = useTokenStore();
+
   return (
     <Layout>
       <NavigationBar
         $isSeparated={false}
-        $isAuthorized={false}
+        $isAuthorized={!!accessToken}
         placeholderText="플레이스 홀더"
+        // TODO : 유저 이미지 파일 경로가 있으면 주석 해제
+        // userProfileSrc={userInfo.profileImageUrl}
       />
       <Suspense fallback={<div>Loading...</div>}>
         <MainContent />
