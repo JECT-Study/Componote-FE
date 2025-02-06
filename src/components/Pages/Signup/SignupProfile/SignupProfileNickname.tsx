94cedf61498181d0b0fd590d09be1c0c750de49d
Jiye Lee <sije3757@snu.ac.kr>
2025-02-06 14:41:47 +0900

    [✨feat]: 회원가입시 닉네임 유효성 검사 로직 추가 (#71)
    Co-authored-by: jiyeeeah <sije3757@gmail.com>

--- src/components/Pages/Signup/SignupProfile/SignupProfileNickname.tsx
+++ src/components/Pages/Signup/SignupProfile/SignupProfileNickname.tsx
@@ -1,10 +1,20 @@
 import { Button, InputField } from "@/components";
 import { ButtonStyle } from "@/components/Button/Button.types";
 import { useSignupUserStore } from "@/hooks/store/useSignupUserStore";
+import { useState } from "react";
+import validateNickname from "@/utils/validateNickname";
 import * as S from "./SignupProfileNickname.style";
 
 export default function SignupProfileNickname() {
   const { nickname, setNickname } = useSignupUserStore();
+  const [isNickNameValid, setIsNickNameValid] = useState(true);
+
+  function handleNicknameChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const input = event.target.value;
+
+    setIsNickNameValid(validateNickname(input));
+    setNickname(input);
+  }
 
   return (
     <S.SignupProfileNicknameInputContainer>
@@ -21,16 +31,19 @@ export default function SignupProfileNickname() {
           </S.SignupProfileNicknameInputLabel>
         }
         $helperVisible
+        // TODO: 유효성 검사에 따라 helperText 기획명세에 맞게 변경
         helperText="특수문자, 공백 제외. 한글, 영문, 숫자로 10자 이내만 가능해요."
         countLimit="10"
         $style={{ flex: "1 0 0" }}
+        $isNegative={!isNickNameValid}
         value={nickname}
-        onChange={(event) => setNickname(event.target.value)}
+        onChange={(event) => handleNicknameChange(event)}
       />
       <Button
         text="중복 검사"
         $size="md"
         $buttonStyle={ButtonStyle.OutlinedPrimary}
+        $isDisabled={!isNickNameValid}
       />
     </S.SignupProfileNicknameInputContainer>
   );
