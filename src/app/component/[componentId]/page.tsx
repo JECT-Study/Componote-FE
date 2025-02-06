28ed871e67cfb7dab1158035b6b9d93ca46e6c96
ccconac <qwerpoiu91@naver.com>
2025-02-06 18:35:43 +0900

    [✨feat]: component detail page 컴포넌트 설명 표시

--- src/app/component/[componentId]/page.tsx
+++ src/app/component/[componentId]/page.tsx
@@ -1,8 +1,27 @@
-import { Layout, NavigationBar, DocumentBanner, Footer } from "@/components";
+"use client";
+
+import {
+  Layout,
+  NavigationBar,
+  DocumentBanner,
+  Footer,
+  EmptyState,
+} from "@/components";
+import { useParams } from "next/navigation";
 import { DocumentSection } from "@/components/Pages";
 import { NAVBAR_ITEM_TEXT } from "@/constants/messages";
+import useComponentDetailQuery from "@/hooks/api/componentDetail/useComponentDetailQuery";
 
 export default function ComponentDetail() {
+  const params = useParams();
+  const componentId = Number(params.componentId);
+
+  const { data, isLoading, isError } = useComponentDetailQuery(componentId);
+
+  if (!data || isError) {
+    return <EmptyState text="컴포넌트 세부 내용을 불러올 수 없어요" />;
+  }
+
   return (
     <Layout>
       <NavigationBar
@@ -11,20 +30,19 @@ export default function ComponentDetail() {
         placeholderText={NAVBAR_ITEM_TEXT.inputPlaceholder}
       />
       <DocumentBanner
-        titleText="아코디언 Accoridion"
-        componentListText="확장 패널 Expansion Panel, 토글 패널 Toggle Panel, 콜랩스 Collapse, 디스클로저 Disclosure, 드롭다운 섹션 Dropdown Section, 디테일 Details, 확장 기능 Expander"
-        bodyText="아코디언은 추가적인 정보를 숨기거나 표시하는 데에 사용되는 컴포넌트예요."
+        $src={data.thumbnailUrl}
+        titleText={data.title}
+        componentListText={data.mixedNames.join(", ")}
+        bodyText={data.introduction}
+        commentCount={data.commentCount.toString()}
+        bookmarkCount={data.commentCount.toString()}
       />
       <DocumentSection
-        designReferenceCount={124}
-        descriptionText="아코디언은 추가 정보를 표시하거나 숨기기 위해 상호작용 가능한
-                  제목들이 수직으로 쌓인 UI 구성 요소입니다. 각 항목은 짧은 라벨만
-                  보이도록 ‘접기’ 상태로 유지되거나, 전체 내용을 보기 위해 ‘펼치기’
-                  상태로 전환될 수 있어요."
-        useCaseText="검색 기능 화면에서 검색 결과가 표시될 때, 항목 별로 아코디언을
-                    사용해 표시합니다. 이때, 동시에 여러 아코디언을 자유롭게 확장하고
-                    축소할 수 있습니다."
+        designReferenceCount={data.designReferenceCount}
+        descriptionText={data.blocks.DESCRIPTION[0].content}
+        useCaseText={data.blocks.USE_CASE[0].content}
       />
+      {isLoading && <EmptyState text="컴포넌트 세부 내용을 로드 중이에요" />}
       <Footer />
     </Layout>
   );
