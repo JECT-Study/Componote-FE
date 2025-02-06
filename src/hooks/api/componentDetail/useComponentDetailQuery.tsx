0179078b212a4cb67024b0fd287634e0a77e51a9
ccconac <qwerpoiu91@naver.com>
2025-02-06 18:34:48 +0900

    [✨feat]: useComponentDetailQuery hook 구현

---
+++ src/hooks/api/componentDetail/useComponentDetailQuery.tsx
@@ -0,0 +1,18 @@
+import { componentDetail } from "@/api/componentDetail";
+import { IComponentDetail } from "@/types/componentDetail";
+import { useQuery } from "@tanstack/react-query";
+
+const useComponentDetailQuery = (componentId: number) => {
+  const fetchComponenetDetail = async () => {
+    const response = await componentDetail(componentId);
+    return response;
+  };
+
+  return useQuery<unknown, Error, IComponentDetail>({
+    queryKey: ["componentDetail", componentId],
+    queryFn: fetchComponenetDetail,
+    enabled: !!componentId,
+  });
+};
+
+export default useComponentDetailQuery;
