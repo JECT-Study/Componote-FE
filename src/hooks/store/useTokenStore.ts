8968eb111196c764972f92f8330f9ee011fbd879
jiyeeeah <sije3757@snu.ac.kr>
2025-02-11 20:55:32 +0900

    [🐛fix]: token 초기화되는 오류 수정
    - accessToken을 store로 관리하니, 새로고침하거나
      페이지 이동시 초기화되는 오류 발견
    - 해당 토큰을 sessionStorage에 저장

--- src/hooks/store/useTokenStore.ts
+++ src/hooks/store/useTokenStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 interface ITokenState {
   accessToken: string | undefined;
@@ -12,10 +13,21 @@ interface ITokenActions {
 }
 
 // eslint-disable-next-line import/prefer-default-export
-export const useTokenStore = create<ITokenState & ITokenActions>((set) => ({
-  accessToken: undefined,
-  memberId: 0,
-  setAccessToken: (accessToken: string) => set({ accessToken }),
-  setMemberId: (memberId: number) => set({ memberId }),
-  logout: () => set({ accessToken: "" }),
-}));
+export const useTokenStore = create<ITokenState & ITokenActions>()(
+  persist(
+    (set) => ({
+      accessToken: undefined,
+      memberId: 0,
+      setAccessToken: (accessToken: string) => set({ accessToken }),
+      setMemberId: (memberId: number) => set({ memberId }),
+      logout: () => {
+        set({ accessToken: "", memberId: 0 });
+        localStorage.removeItem("token-storage");
+      },
+    }),
+    {
+      name: "token-storage",
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
