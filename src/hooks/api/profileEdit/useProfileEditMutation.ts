fa3ef709d8e9cac6afde57954cdd686ba5201c1e
Jiye Lee <sije3757@snu.ac.kr>
2025-02-14 14:11:24 +0900

    [🐛fix]: 프로필 편집 후 프로필 페이지에 userInfo 바로 업데이트 (#88)

--- src/hooks/api/profileEdit/useProfileEditMutation.ts
+++ src/hooks/api/profileEdit/useProfileEditMutation.ts
@@ -1,11 +1,18 @@
 import { putProfileEdit } from "@/api/userInfo";
+import { useUserInfoStore } from "@/store/user/useUserInfoStore";
 import { IProfileEdit } from "@/types/api/user";
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
+import { useTokenStore } from "@/store/user/useTokenStore";
+import { useUserInfoQuery } from "../useUserInfoQuery";
 
 // eslint-disable-next-line import/prefer-default-export
 export const useProfileEditMutation = (accessToken: string) => {
+  const { setUserInfo } = useUserInfoStore();
+  const { memberId } = useTokenStore();
+  const { refetch } = useUserInfoQuery(accessToken, memberId);
   const router = useRouter();
+
   return useMutation({
     mutationFn: async ({
       nickname,
@@ -19,7 +26,11 @@ export const useProfileEditMutation = (accessToken: string) => {
       });
       return response.data;
     },
-    onSuccess: () => {
+    onSuccess: async () => {
+      const { data: newUserInfo } = await refetch();
+      if (newUserInfo) {
+        setUserInfo(newUserInfo);
+      }
       router.push("/profile");
     },
   });
