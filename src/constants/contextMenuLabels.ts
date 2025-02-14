cdf17dc45435b514588983ce8fb777b97aa51b97
jiyeeeah <sije3757@snu.ac.kr>
2025-02-14 10:35:27 +0900

    [✨feat]: 상단바의 Avatar 클릭시 context Menu 구현
    - Avatar 클릭시 contextMenu 뜨도록 구현
    - 마이페이지 클릭시 /profile로 이동
    - 로그아웃 클릭시, accessToken 초기화로 로그아웃 임시 구현

--- src/constants/contextMenuLabels.ts
+++ src/constants/contextMenuLabels.ts
@@ -16,3 +16,10 @@ export const BOOKMARK_CONTEXT_MENU_ITEM_LABELS = [
   "댓글 순으로 정렬",
   "등록 순으로 정렬",
 ];
+
+export const AVATAR_CONTEXT_MENU_ITEM_LABELS = {
+  myPage: "마이 페이지",
+  // TODO : 북마크 기능 추가 시 주석 해제
+  // myBookmark: "내 북마크",
+  logOut: "로그아웃",
+};
